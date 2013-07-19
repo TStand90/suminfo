53afc6cacb2efc5d01363591b2f930f5523c15c9
Andre <andrebautista2513@gmail.com>
2013-07-19 02:23:09 -0700

    Updated Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Suminfo::Application.routes.draw do
-  get "static_pages/about"
-  get "static_pages/portfolio"
-  get "static_pages/contact"
+  root  'static_pages#about'
+  match '/portfolio', to: 'static_pages#portfolio', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
